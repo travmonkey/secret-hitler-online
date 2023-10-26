556d68378274e547f71109bf48578e22334832a8
travmonkey <travishepworth14@gmail.com>
2023-10-26 12:48:02 -0500

    Implement client connection detect on server

--- README.md
+++ README.md
@@ -3,20 +3,26 @@
 WORK IN PROGRESS
 
 ### Clone the REPO
-`git clone https://github.com/travmonkey/secret-hitler-online.git`
-To replicate make sure you have nodeJS installed.
+`git clone https://github.com/travmonkey/secret-hitler-online.git` 
+
+## Installing NODEJS
 ### Arch
 `sudo pacman -S`
+
 
 ### Debian
 `sudo apt-get install nodejs`
 
+
 ### Windows and Mac
 fitfo
+
 
 ## Installing node packages
 `npm install`
 
+
 ### Basic Usage:
 `npm run build` to compile typescript into javascript
+
 `npm run start` to host the server 
--- index.ts
+++ index.ts
@@ -1,7 +1,12 @@
 import express, { Express, Request, Response } from 'express'
 import bodyParser from 'body-parser'
+import { createServer } from 'node:http'
+import { Server } from "socket.io"
 
 const app: Express = express()
+const server = createServer(app)
+const io = new Server(server)
+
 const port: Number = 5002
 
 app.use(bodyParser.urlencoded())
@@ -13,6 +18,14 @@ app.get('/', (req: Request, res: Response) => {
     res.render('../views/pages/index', {})
 })
 
+app.get('/message', (req: Request, res: Response) => {
+    res.render('../views/pages/messaging', {})
+})
+
+io.on('connection', (socket) => {
+    console.log('a user has connected')
+})
+
 app.post('/submit', (req: Request, res: Response) => {
     const name1: string = req.body.name;
     const age1: number = req.body.age;
@@ -20,6 +33,6 @@ app.post('/submit', (req: Request, res: Response) => {
     res.send('Recieved.')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`)
 })
