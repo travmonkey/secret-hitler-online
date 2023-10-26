dba9cd09d92170239bebe9faa2f643357631b96d
travmonkey <travishepworth14@gmail.com>
2023-10-26 11:43:00 -0500

    Reworked API to call without external JS

--- dist/index.js
+++ dist/index.js
@@ -7,14 +7,16 @@ const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
 const port = 5002;
-app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded());
 app.use("/public", express_1.default.static('public'));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-    res.render('../views/pages/index');
+    res.render('../views/pages/index', {});
 });
 app.post('/submit', (req, res) => {
-    console.log('Recieved the data:', req.body);
+    const name1 = req.body.name;
+    const age1 = req.body.age;
+    console.log('Recieved the data:', name1, age1);
     res.send('Recieved.');
 });
 app.listen(port, () => {
--- index.ts
+++ index.ts
@@ -4,17 +4,19 @@ import bodyParser from 'body-parser'
 const app: Express = express()
 const port: Number = 5002
 
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded())
 app.use("/public", express.static('public'))
 
 app.set('view engine', 'ejs')
 
 app.get('/', (req: Request, res: Response) => {
-    res.render('../views/pages/index')
+    res.render('../views/pages/index', {})
 })
 
 app.post('/submit', (req: Request, res: Response) => {
-    console.log('Recieved the data:', req.body)
+    const name1: string = req.body.name;
+    const age1: number = req.body.age;
+    console.log('Recieved the data:', name1, age1)
     res.send('Recieved.')
 })
 
